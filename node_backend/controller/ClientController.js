2294a5468a85cce0a781b9149cded6069348e6c6
Rahul Ramesh Warke <88077569+rahulwarke554@users.noreply.github.com>
2021-08-08 23:06:41 +0530

    Post, Update, delete

--- node_backend/controller/ClientController.js
+++ node_backend/controller/ClientController.js
@@ -4,6 +4,8 @@ var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Client } = require('../models/Client');
 
+//Get Method To show the data.
+
 router.get('/', (req, res) => {
 
     Client.find((err, docs) => {
@@ -12,7 +14,7 @@ router.get('/', (req, res) => {
     });
 
 });
-
+// search Data by Id.
 router.get('/:id', (req, res) => {
     if (!ObjectId.isValid(req.params.id))
         return res.status(400).send('No record with given id : ${req.params.id }');
@@ -22,6 +24,8 @@ router.get('/:id', (req, res) => {
     });
 
 });
+
+//set the Data to server
 
 router.post('/', (req, res) => {
     var client = new Client({
@@ -40,9 +44,51 @@ router.post('/', (req, res) => {
         driving_lic: req.body.driving_lic,
         issue_date: req.body.issue_date
     });
-    client.save((err, doc) => {
+    client.save((err, docs) => {
         if (!err) { res.send(docs); } else { console.log('error in Client save :' + JSON.stringify(err, undefined, 2)); }
     });
+});
+
+// update method
+router.put('/id:',(req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No record with given id : ${req.params.id }');
+
+    var client = {
+        fname: req.body.fname,
+        mname: req.body.mname,
+        lname: req.body.lname,
+        add1: req.body.add1,
+        add2: req.body.add2,
+        email: req.body.email,
+        city: req.body.city,
+        zip: req.body.zip,
+        phone_number: req.body.phone_number,
+        mobile_number: req.body.mobile_number,
+        Credit_Card_type: req.body.Credit_Card_type,
+        card_number: req.body.card_number,
+        driving_lic: req.body.driving_lic,
+        issue_date: req.body.issue_date,
+    };
+
+    Client.findById(req.params.id, { $set: client }, {new :true}, (err, doc) => {
+        if (!err) { res.send(docs); } 
+        else { console.log('error in Client save :' + JSON.stringify(err, undefined, 2)); }
+    });
+
+});
+
+// Delete Operation of Data.
+
+router.delete('/id:',(req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No record with given id : ${req.params.id }');
+
+    Client.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) { res.send(docs); } 
+        else { console.log('error in Client save :' + JSON.stringify(err, undefined, 2)); }
+    });
+
 });
 
 module.exports = router;
