4b2e3f1e1ac9c49567b2e3c2f1ce77204dadb053
bhairavi248 <bhairavibhoge248@gmail.com>
2021-08-27 09:08:10 +0530

    updated Addon

--- react_frontend/src/components/Addon.js
+++ react_frontend/src/components/Addon.js
@@ -3,14 +3,14 @@ import Nav from './Nav'
 import Footer from './Footer'
 import {Switch,Route,Link} from "react-router-dom";
 
-class SignIn extends Component {
+class Addon extends Component {
     constructor(props) {
         super(props);
         console.log("addon",this.props);
         this.state = { 
-        email: '',
-        password: '',
-       
+       nav:"",
+       camp:"",
+       chSeats:""
     }
       }
       mySubmitHandler = (event) => {
@@ -36,13 +36,13 @@ class SignIn extends Component {
                                 <label>password : </label>
                                 <input type="text" className="form-control" name="password" onChange={this.myChangeHandler} ></input> */}
                                 
-                                <input type="checkbox" id="1" name="1" value="B"></input>
+                                <input type="checkbox" id="1" name="nav" value="B" onClick={ async() => { await this.setState({nav:"GPS Navigation System"}); console.log(this.state)}}></input>
                                 &nbsp;&nbsp;&nbsp;&nbsp;
                                 <label for="1"> GPS Navigation System&nbsp;&nbsp; <span>&#36;10.00 /day</span></label><br></br>
-                                <input type="checkbox" id="1" name="1" value="B"></input>
+                                <input type="checkbox" id="1" name="camp" value="B" onClick={ async() => { await this.setState({camp:"Camping Kit"}); console.log(this.state)}}></input>
                                 &nbsp;&nbsp;&nbsp;&nbsp;
                                 <label for="1"> Camping Kit &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span>&#36;30.00 /day</span></label><br></br>
-                                <input type="checkbox" id="1" name="1" value="B"></input>
+                                <input type="checkbox" id="1" name="chSeats" value="B" onClick={ async() => { await this.setState({chSeats:"Child Seats"}); console.log(this.state)}}></input>
                                 &nbsp;&nbsp;&nbsp;&nbsp;
                                 <label for="1"> Child Seats &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span>&#36;2.00 /day(For additonal seat 1 seat free )</span></label><br></br>
                                 &nbsp;&nbsp;&nbsp;&nbsp;
@@ -64,4 +64,4 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn;
+export default Addon;
